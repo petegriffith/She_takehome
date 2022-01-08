c5ad3c518f67004fe3007c936945f4471628af41
Peter <griffitp12@gmail.com>
2022-01-08 14:59:15 +0900

    More scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import Header from './components/Header'
 import SingleTodo from './components/SingleTodo'
@@ -6,16 +6,18 @@ import TodoInput from './components/TodoInput'
 
 import './styles/App.css'
 
+import data from './assets/initData.json'
+
 function App() {
-  const todos = ['Buy Milk', 'Drink Milk', 'Buy More Milk']
+  const [currentTodos, setCurrentTodos] = useState(data)
 
   return (
     <>
       <Header />
       <div className="App_inner-container">
         <TodoInput />
-        <div className="App_todos_container">
-            {todos.map((todo) => <SingleTodo todoName={todo} />)}
+        <div className="App_todos-container">
+          {currentTodos.map((todo, index) => <SingleTodo todo={todo} />)}
         </div>
       </div>
     </>
--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React from 'react'
 
-function SingleTodo({todoName}:{todoName: string}) {
-    return(
-        <div>
-            <p>{todoName}</p>
-        </div>
-    )
+import '../styles/SingleTodo.css'
+import '../styles/_flex.css'
+
+interface Todo {
+  task: string
+  completed: boolean
+}
+
+function SingleTodo({ todo }: { todo: Todo }) {
+  return (
+    <div className="_flex-row todo-container">
+      <div className="_flex-row">
+        <input type="checkbox" />
+        <p>{todo.task}</p>
+      </div>
+      <div className="_flex-row button-container">
+        <div className="button delete-button">Delete</div>
+        <div className="button edit-button">Edit</div>
+      </div>
+    </div>
+  )
 }
 
 export default SingleTodo
